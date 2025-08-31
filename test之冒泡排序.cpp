ccad98101633a9552f1d0f0e57b124cc7534f892
seasky-right <seasky23333@gmail.com>
2025-09-01 01:23:34 +0800

    Add files via upload

---
+++ test之冒泡排序.cpp
@@ -0,0 +1,31 @@
+/*#include <iostream>
+using namespace std;
+int main(){
+	int arr[10] = { 5,2,1,4,3,9,6,7,8,0 };
+
+	cout << "排序前的数组为：" << endl;
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << " ";
+	}
+
+	int a;
+	for (int j = 0; j < 10; j++) {
+		for (int k = 0; k < 9; k++) {
+			if (arr[k] > arr[k + 1]) {
+				a = arr[k];
+				arr[k] = arr[k + 1];
+				arr[k + 1] = a;
+			}
+			
+		}
+	}
+
+	cout << "排序后的数组为：" << endl;
+	for (int l = 0; l < 10; l++) {
+		cout << arr[l] << " ";
+	}
+
+	system("pause");
+	return 0;
+}
+*/
